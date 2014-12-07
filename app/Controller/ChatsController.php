8e3f569fe64d4babc1f562e172fb6c234761a81d
Anik <Anik@Anik-PC>
2014-12-07 22:28:33 +0530

    Controller for chat page

---
+++ app/Controller/ChatsController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ChatsController
+ *
+ * @author Anik
+ */
+class ChatsController extends AppController{
+    
+    public $layout = 'dashboard';
+
+
+    public function beforeFilter() {
+        parent::beforeFilter();
+    }
+    
+    public function index(){
+        
+    }
+}
+
+?>
