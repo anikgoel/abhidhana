3e990d69114f042018df77ba0fa3fc720a599961
Atul Tagra <atul.tagra.89@gmail.com>
2014-12-07 22:40:48 +0530

    Added function to fetch all logged in users

--- app/Controller/ChatsController.php
+++ app/Controller/ChatsController.php
@@ -22,6 +22,39 @@ class ChatsController extends AppController{
     public function index(){
         
     }
+    
+    /**
+     * Function to return all users details when joining the room
+     */
+    public function getUserDetails(){
+        $this->autoRender = false;
+   
+        $users = array();
+        
+        $user_ids = $this->request->query('uuids');
+        
+        if(!empty($user_ids)){
+            $this->loadModel('User');
+            
+            $users = $this->User->find('all', array(
+                    'conditions' => array(
+                        'User.id' => $user_ids
+                    ),
+                    'fields' => array(
+                        'User.id', 'User.name', 'User.username'
+                    )
+                )
+            );
+        }
+        
+        if(!empty($users)){
+            $res = array('message' => 'Users found', 'data' => $users);
+        }else{
+            $res = array('message' => 'No users found');
+        }
+        
+        echo json_encode($res);
+    }
 }
 
 ?>
