ef6708d843646432c686d8cac5fc80d50f15e70a
Atul Tagra <atul.tagra.89@gmail.com>
2014-12-07 22:39:13 +0530

    Model files

---
+++ app/Model/Channel.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppModel', 'Model');
+
+/**
+ * CakePHP Channel
+ * @author Atul Tagra <atul.tagra.89@gmail.com>
+ */
+class Channel extends AppModel {
+    
+}
