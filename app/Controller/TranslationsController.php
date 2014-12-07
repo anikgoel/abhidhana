147423e29d6c3ae963a618c18118916751b74ed2
Anik <Anik@Anik-PC>
2014-12-07 22:28:15 +0530

    function to translate

--- app/Controller/TranslationsController.php
+++ app/Controller/TranslationsController.php
@@ -17,9 +17,10 @@ class TranslationsController extends AppController {
     public function translateData() {
         $this->autoRender = False;
         $data = $this->request->data;
-        pr($data);
-        die;
-        $translated_text = $this->Translation->translateText("what you are doing?", "it", "en");
+//        pr($data);
+//        die;
+        $translated_text = $this->Translation->translateText($data['text'], $data['language_to'], $data['language_from']);
+        echo json_encode(array('status'=>1,'data'=>$translated_text));
     }
 
 }
